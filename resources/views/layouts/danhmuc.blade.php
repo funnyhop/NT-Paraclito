1bd957899f1b3b051a11a3005777145b5aca8dd0
Thai Hop <funnyhop01@gmail.com>
2023-08-24 10:34:57 +0700

    second commit

--- resources/views/layouts/danhmuc.blade.php
+++ resources/views/layouts/danhmuc.blade.php
@@ -3,28 +3,25 @@
         <ul>
             <li><a href="">Tổng quan</a></li>
             <li><a href="">Bán hàng</a></li>
-            <li><a href="">Quản lý hóa đơn</a></li>
-            <li><a class="dropdown-toggle" data-bs-toggle="dropdown" href="">Quản lý kho</a>
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="">Nhập từ nhà cung cấp</a></li>
-                    <li><a class="dropdown-item" href="">Nhập tồn</a></li>
-                    <li><a class="dropdown-item" href="">Tồn kho</a></li>
-                    <li><a class="dropdown-item" href="">Kiểm kho</a></li>
-                </ul>
-            </li>
-            <li class="">
-                <a class="d-inline-flex align-items-center rounded" data-bs-toggle="collapse" data-bs-target="#layout-collapse" aria-expanded="true" aria-current="true">
-                  Layout
-                </a>
+            <li>
+                <a class="d-inline-flex align-items-center rounded" data-bs-toggle="collapse" data-bs-target="#layout-collapse"
+                     aria-expanded="true" aria-current="true">Quản hóa đơn</a>
                 <div class="collapse show" id="layout-collapse">
                     <ul class="list-unstyled fw-normal pb-1 small">
-                      <li><a href="" class="d-inline-flex align-items-center rounded">Breakpoints</a></li>
-                      <li><a href="" class="d-inline-flex align-items-center rounded">Containers</a></li>
-                      <li><a href="" class="d-inline-flex align-items-center rounded">Grid</a></li>
-                      <li><a href="" class="d-inline-flex align-items-center rounded">Columns</a></li>
-                      <li><a href="" class="d-inline-flex align-items-center rounded">Gutters</a></li>
-                      <li><a href="" class="d-inline-flex align-items-center rounded">Utilities</a></li>
-                      <li><a href="" class="d-inline-flex align-items-center rounded">Z-index</a></li>
+                      <li><a href="" class="d-inline-flex align-items-center rounded">Đơn xuất</a></li>
+                      <li><a href="" class="d-inline-flex align-items-center rounded">Đơn nhập</a></li>
+                    </ul>
+                </div>
+            </li>
+            <li class="">
+                <a class="d-inline-flex align-items-center rounded" data-bs-toggle="collapse" data-bs-target="#layout-collapse"
+                     aria-expanded="true" aria-current="true">Quản lý kho</a>
+                <div class="collapse show" id="layout-collapse">
+                    <ul class="list-unstyled fw-normal pb-1 small">
+                      <li><a href="" class="d-inline-flex align-items-center rounded">Nhập từ nhà cung cấp</a></li>
+                      <li><a href="" class="d-inline-flex align-items-center rounded">Nhập tồn</a></li>
+                      <li><a href="" class="d-inline-flex align-items-center rounded">Tồn kho</a></li>
+                      <li><a href="" class="d-inline-flex align-items-center rounded">Kiểm kho</a></li>
                     </ul>
                 </div>
             </li>
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -9,33 +9,38 @@
                         width="45"
                         padding: 0px
                         alt="">
-                    <p class="fs-6 text-light fw-bold d-flex py-3">NhaThuoc PARACLITO</p>
+                    <a class="fs-6 text-light fw-bold d-flex py-3" href="">NhaThuoc PARACLITO</a>
                 </div>
             </div>
         </div>
         <div class="col-9">
             <div class="row hder">
-                <div class="col-7">
-                    <div class="input-group pt-2">
+                <div class="col-5">
+                    {{-- <div class="input-group pt-2">
                         <input type="text" class="form-control" placeholder="Tìm kiếm sản phẩm">
                         <span class="input-group-text" id="basic-addon2">
-                            <i class="fa-solid fa-magnifying-glass"></i>
+                            <a href=""><i class="fa-solid fa-magnifying-glass"></i></a>
                         </span>
-                    </div>
+                    </div> --}}
                 </div>
-                <div class="col-2">
-                    <div class="fs-3 float-end pt-2">
-                        <i class="fa-solid fa-bag-shopping fa-shake" style="color: #e6eff5;"></i>
-                    </div>
+                <div class="col-3 pt-3">
+                    <input class="float-end" type="date" style="background-color: #e6eff5; border-radius:3px"id="date">
                 </div>
-                <div class="col-3">
+                <div class="col-4">
                     <div class="row py-2">
-                        <div class="col-8 text-white">
-                            <p class="fw-normal">Xin chào!</p>
-                            <p class="fw-bold">Đăng nhập</p>
+                        <div class="col-2 fs-3 pt-1 bg-waring">
+                            <i class="fa-solid fa-bag-shopping fa-shake" style="color: #e6eff5;"></i>
                         </div>
-                        <div class="col-4 fs-3 pt-1 ps-4">
-                            <i class="fa-solid fa-user-nurse" style="color: #e6eff5;"></i>
+                        <div class="col-7 text-white">
+                            <p class="fw-normal">Xin chào!</p>
+                            <a class="fw-bold" href="">Đăng nhập</a>
+                        </div>
+                        <div class="col-3 fs-3 pt-1 ps-4">
+                            <i class="fa-solid fa-user-nurse" data-bs-toggle="dropdown" style="color: #e6eff5;"></i>
+                            <ul class="dropdown-menu bg-gray-100">
+                                <li class="dropdown-item">Đăng ký</li>
+                                <li class="dropdown-item">Đăng xuất</li>
+                            </ul>
                         </div>
                     </div>
                 </div>
