6270e8e3ab03604c983231614471a3502078ef53
Thai Hop <funnyhop01@gmail.com>
2023-11-23 18:11:53 +0700

    November 23 2

--- resources/views/sales/index.blade.php
+++ resources/views/sales/index.blade.php
@@ -23,159 +23,99 @@
         <!-- Main content -->
         <div class="content">
             <div class="container-fluid">
-                <div class="row pl-5 pt-2 border-right">
-                    <div class="col-4">
-                        <b>Thêm khách hàng:</b>
-                    </div>
+                <div class="row pl-4 pt-2 border-right">
                     <div class="col-4 pl-2 pr-2">
                         <b>Thêm hóa đơn:</b>
                     </div>
-                    <div class="col-4">
-                        <b>Ghi đơn thuốc:</b>
-                    </div>
                 </div>
-                <div class="row pt-1 pl-5">
-                    <div class="col-4">
+                <div class=" pt-1 pl-2">
+                    <div class="row pl-4">
                         <form action="{{ route('sales') }}" method="post">
                             @csrf
-                            <div class="input-group d-flex pb-2">
-                                <label for="exampleInput1">Mã khách hàng:</label>
-                                <input type="text" class="input-form" name="id" value="{{ $newKHID }}" id="exampleInput1"
-                                    placeholder="KH001">
+                            <div class="d-flex ">
+                                <div class="input-group d-flex pb-2 pr-3">
+                                    <label for="exampleInput1">Mã hóa đơn:</label>
+                                    <input type="text" class="input-form" name="idhd" value="{{ $newHDID }}" id="exampleInput1" placeholder="HD001">
+                                </div>
+                                <div class="input-group d-flex pb-2 pr-3">
+                                    <label for="manv">Nhân viên lập hóa đơn:</label>
+                                    {{-- <input type="text" class="input-form" id="exampleInput1" placeholder="BT001"> --}}
+                                    <select class="input-select pl-2" name="staff_id" id="manv">
+                                        <option value="{{ Auth::check() && Auth::user()->NVID ? Auth::user()->NVID : '' }}">{{ Auth::user()->TenNV }}</option>
+                                        {{-- <option selected disabled>Chọn nhân viên</option> --}}
+                                        {{-- @foreach ($staffs as $staff)
+                                            <option value="{{ $staff->NVID }}">{{ $staff->TenNV }}</option>
+                                        @endforeach --}}
+                                    </select>
+                                </div>
+                                <div class="input-group d-flex pb-2 pr-3">
+                                    <label for="exampleInput1">Toa thuốc:</label>
+                                    {{-- <input type="text" class="input-form" id="exampleInput1"
+                                        placeholder="T0000/không có toa"> --}}
+                                    <select class="input-select pl-2" name="prescription_id" id="manv">
+                                        <option value="" selected disabled>Chọn toa thuốc</option>
+                                        @foreach ($pres as $pre)
+                                            <option value="{{ $pre->ToaID }}">{{ $pre->ToaID }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                                <div class="input-group d-flex pb-2 pr-3">
+                                    <label for="exampleInput1">Mã khách hàng:</label>
+                                    {{-- <input type="text" class="input-form" id="exampleInput1" placeholder="KH001"> --}}
+                                    <select class="input-select pl-2" name="customer_id" id="mat">
+                                        <option selected disabled>Chọn mã KH</option>
+                                        @foreach ($customers as $customer)
+                                            <option value="{{ $customer->KHID }}">{{ $customer->KHID }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                                <div class="input-group d-flex pb-2 pr-3">
+                                    <label for="exampleInput1">Đối tượng sử dụng:</label>
+                                    <input type="text" class="input-form" name="DoituongSD" id="exampleInput1" placeholder="TE001">
+                                </div>
                             </div>
-                            <div class="input-group d-flex pb-2">
-                                <label for="exampleInput1">Tên khách hàng:</label>
-                                <input type="text" class="input-form" name="name" id="exampleInput1"
-                                    placeholder="Nguyen Van A">
+                            <div class="float-right pt-2 pr-3">
+                                <button type="reset" class="btn btn-secondary">Hủy</button>
+                                <button type="submit" class="btn btn-primary">Thêm</button>
                             </div>
-                            <div class="input-group d-flex pb-2">
-                                <label for="exampleInput1">Số điện thoại:</label>
-                                <input type="text" class="input-form" name="sdt" id="exampleInput1"
-                                    placeholder="039287593">
+                        </form>
+                    </div>
+                    <div class="row pl-4">
+                        <form action="{{ route('sales') }}" method="post">
+                            @csrf
+                            <div>
+                                <b>Ghi đơn thuốc:</b>
                             </div>
-                            <div class="input-group d-flex pb-2">
-                                <label for="exampleInput1">Địa chỉ:</label>
-                                <input type="text" class="input-form" name="address" id="exampleInput1"
-                                    placeholder="Ninh Kieu-CT">
+                            <div class="d-flex ">
+                                <div class="input-group d-flex pb-2 pr-3">
+                                    <label for="exampleInput1">Mã hóa đơn:</label>
+                                    {{-- <input type="text" class="input-form" id="exampleInput1" placeholder="HD001"> --}}
+                                    <select class="input-select pl-2" name="bill_id" id="mat">
+                                        <option selected disabled>Chọn HD</option>
+                                        @foreach ($bills as $bill)
+                                            <option value="{{ $bill->HDID }}">{{ $bill->HDID }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                                <div class="input-group d-flex pb-2 pr-3">
+                                    <label for="mat">Mã thuốc:</label>
+                                    {{-- <input type="text" class="input-form" id="exampleInput1" placeholder="TKV01"> --}}
+                                    <select class="input-select pl-2" name="medicine_id" id="mat">
+                                        <option selected disabled>Chọn thuốc</option>
+                                        @foreach ($drs as $dr)
+                                            <option value="{{ $dr->ThuocID }}">{{ $dr->Tenthuoc }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                                <div class="input-group d-flex pb-2 ">
+                                    <label for="exampleInput1">Số lượng:&nbsp;&nbsp;<i>(viên)</i></label>
+                                    <input type="text" class="input-form" name="sl" id="exampleInput1" placeholder="1">
+                                </div>
                             </div>
                             <div class="float-right pt-2">
-                                <button type="reset" class="btn btn-secondary">Hủy</button>
-                                <button type="submit" class="btn btn-primary">Thêm</button>
+                                <button type="submit" class="btn btn-primary">Ghi</button>
                             </div>
                         </form>
-                    </div>
-                    <div class="col-4 pl-2 pr-2">
-                        <form action="{{ route('sales') }}" method="post">
-                            @csrf
-                            <div class="input-group d-flex pb-2">
-                                <label for="exampleInput1">Mã hóa đơn:</label>
-                                <input type="text" class="input-form" name="idhd" value="{{ $newHDID }}" id="exampleInput1" placeholder="HD001">
-                            </div>
-                            <div class="input-group d-flex pb-2">
-                                <label for="exampleInput1">Toa thuốc:</label>
-                                {{-- <input type="text" class="input-form" id="exampleInput1"
-                                    placeholder="T0000/không có toa"> --}}
-                                <select class="input-select pl-2" name="prescription_id" id="manv">
-                                    <option value="" selected disabled>Chọn toa thuốc</option>
-                                    @foreach ($pres as $pre)
-                                        <option value="{{ $pre->ToaID }}">{{ $pre->ToaID }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                            <div class="input-group d-flex pb-2">
-                                <label for="manv">Mã nhân viên lập hóa đơn:</label>
-                                {{-- <input type="text" class="input-form" id="exampleInput1" placeholder="BT001"> --}}
-                                <select class="input-select pl-2" name="staff_id" id="manv">
-                                    <option value="{{ Auth::check() && Auth::user()->NVID ? Auth::user()->NVID : '' }}">{{ Auth::user()->TenNV }}</option>
-                                    {{-- <option selected disabled>Chọn nhân viên</option> --}}
-                                    {{-- @foreach ($staffs as $staff)
-                                        <option value="{{ $staff->NVID }}">{{ $staff->TenNV }}</option>
-                                    @endforeach --}}
-                                </select>
-                            </div>
-                            <div class="input-group d-flex pb-2">
-                                <label for="exampleInput1">Mã khách hàng:</label>
-                                {{-- <input type="text" class="input-form" id="exampleInput1" placeholder="KH001"> --}}
-                                <select class="input-select pl-2" name="customer_id" id="mat">
-                                    <option selected disabled>Chọn mã KH</option>
-                                    @foreach ($customers as $customer)
-                                        <option value="{{ $customer->KHID }}">{{ $customer->KHID }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                            <div class="input-group d-flex pb-2">
-                                <label for="exampleInput1">Đối tượng sử dụng:</label>
-                                <input type="text" class="input-form" name="DoituongSD" id="exampleInput1" placeholder="TE001">
-                            </div>
-                            <div class="float-right pt-2 pb-5">
-                                <button type="reset" class="btn btn-secondary">Hủy</button>
-                                <button type="submit" class="btn btn-primary">Thêm</button>
-                            </div>
-                        </form>
-                    </div>
-                    <div class="col-4 pr-5 pb-5">
-                        <form action="{{ route('sales') }}" class="pb-1" method="post">
-                            @csrf
-                            <div class="row">
-                                <div class="col-6">
-                                    <div class="input-group d-flex pb-2">
-                                        <label for="exampleInput1">Mã hóa đơn:</label>
-                                        {{-- <input type="text" class="input-form" id="exampleInput1" placeholder="HD001"> --}}
-                                        <select class="input-select pl-2" name="bill_id" id="mat">
-                                            <option selected disabled>Chọn HD</option>
-                                            @foreach ($bills as $bill)
-                                                <option value="{{ $bill->HDID }}">{{ $bill->HDID }}</option>
-                                            @endforeach
-                                        </select>
-                                    </div>
-                                    <div class="input-group d-flex pb-2">
-                                        <label for="exampleInput1">Số lượng:&nbsp;&nbsp;<i>(viên)</i></label>
-                                        <input type="text" class="input-form" name="sl" id="exampleInput1" placeholder="1">
-                                    </div>
-                                </div>
-                                <div class="col-6">
-                                    <div class="input-group d-flex pb-2">
-                                        <label for="mat">Mã thuốc:</label>
-                                        {{-- <input type="text" class="input-form" id="exampleInput1" placeholder="TKV01"> --}}
-                                        <select class="input-select pl-2" name="medicine_id" id="mat">
-                                            <option selected disabled>Chọn thuốc</option>
-                                            @foreach ($drs as $dr)
-                                                <option value="{{ $dr->ThuocID }}">{{ $dr->Tenthuoc }}</option>
-                                            @endforeach
-                                        </select>
-                                    </div>
-                                    <div class="pt-1 mt-4 pb-1 float-right ">
-                                        <button type="submit" class="btn btn-primary">Ghi</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
-                        {{-- <form action="/pre_create" method="post">
-                            <div class="pb-1"><b>Thêm toa thuốc:</b></div>
-                            @csrf
-                            <div class="input-group d-flex pb-2">
-                                <label for="exampleInput1">Mã toa thuốc:</label>
-                                <input type="text" class="input-form" name="id" id="exampleInput1" placeholder="T0001">
-                            </div>
-                            <div class="input-group d-flex pb-2">
-                                <label for="exampleInput1">Tên bác sĩ:</label>
-                                <input type="text" class="input-form" name="bs" id="exampleInput1" placeholder="Nguyen Van A">
-                            </div>
-                            <div class="input-group d-flex pb-2">
-                                <label for="exampleInput1">Tên bệnh viện</label>
-                                <input type="text" class="input-form" name="bv" id="exampleInput1"
-                                    placeholder="Bệnh viện Hoàn Mỹ Cần Thơ">
-                            </div>
-                            <div class="input-group d-flex pb-2">
-                                <label for="exampleInput1">Ngày tạo</label>
-                                <input type="text" class="input-form" name="nt" id="exampleInput1"
-                                    placeholder="2023-10-12">
-                            </div>
-                            <div class="float-right pt-2 pb-5">
-                                <button type="reset" class="btn btn-secondary">Hủy</button>
-                                <button type="submit" class="btn btn-primary">Thêm</button>
-                            </div>
-                        </form> --}}
                     </div>
                 </div>
                 <!-- /.row -->
